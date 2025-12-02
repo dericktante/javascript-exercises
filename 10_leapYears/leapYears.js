9d38e6a279c6b5b891cc0ef278f418d90353be67
dericktante <156044704+dericktante@users.noreply.github.com>
2025-12-02 06:12:07 -0600

    Complete Task 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleBy4 = year % 4 === 0;
+    const isDivisibleBy100 = year % 100 === 0;
+    const isDivisibleBy400 = year % 400 === 0;
 
+    if (isDivisibleBy4
+        && (!isDivisibleBy100 || isDivisibleBy400)){
+         return true; 
+        } else {
+        return false;
+        }
 };
 
 // Do not edit below this line
